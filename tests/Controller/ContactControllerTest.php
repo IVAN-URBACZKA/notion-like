321badb07028e0246375dc5c2cdef46dec10e725
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2024-02-09 10:02:39 +0100

    Functional test form contact

---
+++ tests/Controller/ContactControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Controller;
+use App\Repository\UserRepository;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ContactControllerTest extends WebTestCase
+{
+
+    public function testContactAdd(){
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('heloise.medhurst@halvorson.com');
+        $client->loginUser($testUser);
+        $client->request('GET', '/contact/add');
+    
+        $this->assertResponseIsSuccessful();
+       
+
+    }
+
+       public function testCommentSubmission(){
+  
+         $client = static::createClient();
+         $userRepository = static::getContainer()->get(UserRepository::class);
+         $testUser = $userRepository->findOneByEmail('heloise.medhurst@halvorson.com');
+         $client->loginUser($testUser);
+         $client->request('GET', '/contact/add');
+         $client->submitForm('Submit', [
+            'contact[name]' => 'Jbl',
+            'contact[firstName]' => 'kilucru',
+            'contact[email]' => 'jblol@gmail.com',
+            'contact[tel]' => '0321225566',
+        ]);
+        $this->assertResponseRedirects();
+        
+        
+    }
+
+
+}
